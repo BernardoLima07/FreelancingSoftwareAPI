133eaf4ad887575bf4f67beec883afdb7183ea52
Bernardo <obernardolima@gmail.com>
2024-04-10 22:01:28 -0300

    latest details

--- src/seed/seed.js
+++ src/seed/seed.js
@@ -3,21 +3,49 @@ import { ContractorModel } from "../database/models/contractorModel.js";
 import { JobModel } from "../database/models/jobModel.js";
 import { ContractModel } from "../database/models/contractModel.js";
 import { sequelize } from "../database/config.js";
+import bcrypt from "bcrypt";
 
 export async function seed() {
   try {
     await sequelize.sync({ force: true });
 
+    const salt = await bcrypt.genSalt(12);
+    const passwordJoao = "123456";
+    const passwordMaria = "abcdef";
+    const passwordPedro = "789012";
+    const passwordAna = "uvwxyz";
+
+    const passwordHashClientJoao = await bcrypt.hash(passwordJoao, salt);
+    const passwordHashClientMaria = await bcrypt.hash(passwordMaria, salt);
+    const passwordHashContractorPedro = await bcrypt.hash(passwordPedro, salt);
+    const passwordHashContractorAna = await bcrypt.hash(passwordAna, salt);
+
     const clients = [
-      { name: "João", email: "joao@example.com", password: "123456" },
-      { name: "Maria", email: "maria@example.com", password: "abcdef" },
+      {
+        name: "João",
+        email: "joao@example.com",
+        password: passwordHashClientJoao,
+      },
+      {
+        name: "Maria",
+        email: "maria@example.com",
+        password: passwordHashClientMaria,
+      },
     ];
 
     await Promise.all(clients.map((client) => ClientModel.create(client)));
 
     const contractors = [
-      { name: "Pedro", email: "pedro@example.com", password: "789012" },
-      { name: "Ana", email: "ana@example.com", password: "uvwxyz" },
+      {
+        name: "Pedro",
+        email: "pedro@example.com",
+        password: passwordHashContractorPedro,
+      },
+      {
+        name: "Ana",
+        email: "ana@example.com",
+        password: passwordHashContractorAna,
+      },
     ];
 
     await Promise.all(
@@ -42,8 +70,20 @@ export async function seed() {
     await Promise.all(jobs.map((job) => JobModel.create(job)));
 
     const contracts = [
-      { client_id: 1, contractor_id: 1, job_id: 1, start_date: new Date(), end_date: "2024-05-12"},
-      { client_id: 2, contractor_id: 2, job_id: 2, start_date: new Date(), end_date: "2024-06-25"},
+      {
+        client_id: 1,
+        contractor_id: 1,
+        job_id: 1,
+        start_date: new Date(),
+        end_date: "2024-05-12",
+      },
+      {
+        client_id: 2,
+        contractor_id: 2,
+        job_id: 2,
+        start_date: new Date(),
+        end_date: "2024-06-25",
+      },
     ];
 
     await Promise.all(
