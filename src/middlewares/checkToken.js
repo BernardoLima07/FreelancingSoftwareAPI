2021d4eb61c3a41f3be89ddec7337474fa87b830
BernardoLima07 <obernardolima@gmail.com>
2025-11-20 20:54:40 -0300

    access.

--- src/middlewares/checkToken.js
+++ src/middlewares/checkToken.js
@@ -7,7 +7,7 @@ export const checkToken = (req, res, next) => {
   const token = authHeader && authHeader.split(' ')[1]
 
   if (!token) {
-    return res.status(401).json({ msg: 'Access denied for you.' })
+    return res.status(401).json({ msg: 'Access denied.' })
   }
 
   try {
